2b39186f867049b15673e470d094d4240dc5080e
ShoutGo <857428258@qq.com>
2019-01-24 09:25:03 +0800

    README

--- main.c
+++ main.c
@@ -9,11 +9,14 @@
 int main(int argc, char **argv) { 
 	mytool1_print(33,66,7); 
 	mytool2_print("world");
+
 	sleep(1);
 	printf("\033[1;33;44m%s\033[0m\n","hello world");
+
 	int i;
-	for (i=0;i<10;i++)
+	for (i=0;i<10;i ++)
 		printf("%d",i);
         printf("\n");
+
 	return 0; 
 } 
